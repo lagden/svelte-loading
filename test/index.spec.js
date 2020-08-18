b2176732b541a71109943b55cf4e0aca18e58905
Thiago Lagden <lagden@gmail.com>
2020-08-18 17:24:09 -0300

    fix test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,12 @@
 import {cleanup, render} from '@testing-library/svelte'
-import {Loading} from '../src'
+import {Loading, acts} from '../src'
 
 describe('Loading', () => {
 	afterEach(cleanup)
 
 	test('should match snapshot', () => {
+		acts.show(true)
+
 		const {container} = render(Loading)
 		expect(container).toMatchSnapshot()
 	})
